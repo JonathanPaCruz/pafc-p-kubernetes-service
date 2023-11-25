a1d9f4f39b41aecf83d47d30cce405b01c3cbc18
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-24 22:08:40 -0600

    Initial Commit

---
+++ pafc-c-user-service/src/main/java/com/jodatrash/springcloud/user/pafccuserservice/PafcCUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jodatrash.springcloud.user.pafccuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafcCUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafcCUserServiceApplication.class, args);
+	}
+
+}
---
+++ pafc-c-user-service/src/test/java/com/jodatrash/springcloud/user/pafccuserservice/PafcCUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodatrash.springcloud.user.pafccuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafcCUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
