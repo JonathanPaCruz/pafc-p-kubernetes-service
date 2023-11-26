afaaf7d9eaddb9062cbccfda1661e1b437356f6d
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-25 19:50:28 -0600

    feat: add project structure

---
+++ pafc-c-course-service/src/test/java/com/jodatrash/spring/cloud/course/pafcccourseservice/PafcCCourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodatrash.spring.cloud.course.pafcccourseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafcCCourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
