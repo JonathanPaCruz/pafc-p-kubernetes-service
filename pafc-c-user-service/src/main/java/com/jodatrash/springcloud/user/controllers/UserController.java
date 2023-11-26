d1bcdeab744bcf61ec3683c5c8893f02a15104bb
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-25 19:05:57 -0600

    feat: Add class UserController.java

---
+++ pafc-c-user-service/src/main/java/com/jodatrash/springcloud/user/controllers/UserController.java
@@ -0,0 +1,70 @@
+package com.jodatrash.springcloud.user.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jodatrash.springcloud.user.entity.Users;
+import com.jodatrash.springcloud.user.services.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/private/v1/info")
+    public List<Users> view() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/private/v1/info/{id}")
+    public ResponseEntity<?> detailUsers(@PathVariable Long id) {
+        Optional<Users> userOptional = userService.byId(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok().body(userOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/private/v1/create")
+    // @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody Users user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
+    }
+
+    @PutMapping("/private/v1/edit/{id}")
+    public ResponseEntity<?> edit(@RequestBody Users user, @PathVariable Long id) {
+        Optional<Users> userOptional = userService.byId(id);
+        if (userOptional.isPresent()) {
+            Users userDb = userOptional.get();
+            userDb.setName(user.getName());
+            userDb.setEmail(user.getEmail());
+            userDb.setPassword(user.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/private/v1/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Users> userOptional = userService.byId(id);
+        if (userOptional.isPresent()) {
+            userService.delete(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
